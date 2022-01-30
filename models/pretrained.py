9cd8a9d1617dad4e4078a83e41e18fcc166e9a2e
Shivansh Rajolia <Shivansh_Rajolia@intuit.com>
2022-01-30 06:36:10 +0530

    Adding trained model weights + model changes

--- models/pretrained.py
+++
@@ -1,4 +0,0 @@
-pretrained_models = {
-    "srresnet_bicubic_x4": "https://image-super-resolution-weights.s3.af-south-1.amazonaws.com/srresnet_bicubic_x4/generator.h5",
-    "srgan_bicubic_x4": "https://image-super-resolution-weights.s3.af-south-1.amazonaws.com/srgan_bicubic_x4/generator.h5",
-}
